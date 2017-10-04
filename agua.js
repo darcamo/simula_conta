c38725281047d9241a2e205433c8103bd7065142
Darlan Cavalcante Moreira <darcamo@gmail.com>
2017-10-04 07:58:30 -0300

    update

---
+++ agua.js
@@ -0,0 +1,120 @@
+"use strict";
+
+var precos_agua = [3.16, 4.09, 4.43, 7.58, 13.39];
+var precos_esgoto = [3.49, 4.48, 4.85, 8.33, 14.72];
+
+
+/**
+ * Quebra 'consumo' (em metros cúbicos) nas diversas faixas.
+ *
+ * @param consumo O consumo em m3.
+ * @param minimo O valor do consumo mínimo.
+ *
+ * @return Um array com o consumo em cada faixa.
+ */
+function quebra_faixas(consumo, minimo) {
+    consumo = Math.floor(consumo);
+    minimo = Math.floor(minimo);
+    if(consumo < minimo) {
+        consumo = minimo;
+    }
+
+    let faixas = Array(5).fill(0);
+    let limites = [10, 5, 5, 30, 10000000];
+
+    let idx = 0;
+    while(consumo > 0) {
+        if (consumo > limites[idx]) {
+            faixas[idx] = limites[idx];
+        }
+        else {
+            faixas[idx] = consumo;
+        }
+        consumo = consumo - limites[idx];
+        idx++;
+    }
+    return faixas;
+}
+
+
+
+/**
+ * Calcula o custo de água dado um consumo em m3 e o consumo mínimo de água.
+ *
+ * @param consumo Consumo de água em metros cúbicos.
+ * @param consumo_minimo_agua Consumo mínimo de água.
+ *
+ * @return Custo em reais de água separado por faixa.
+ */
+function calcula_custo_agua_por_faixa(consumo, consumo_minimo_agua) {
+    let faixas = quebra_faixas(consumo, consumo_minimo_agua);
+    return multiplicaArrays(faixas, precos_agua);
+}
+
+
+/**
+ * Calcula a taxa de contingência a ser paga.
+ *
+ * @param consumo Consumo em m3
+ * @param consumo_minimo_agua Consumo mínimo em m3.
+ * @param meta Meta de consumo para não pagar a tarifa em m3.
+ *
+ * @return Valor da taxa de contingência separado por faixa de consumo.
+ */
+function calcula_custo_agua_excedente_por_faixa(consumo, consumo_minimo_agua, meta) {
+    let contingencia_por_faixas;
+    if (consumo < meta) {
+        contingencia_por_faixas = Array(5).fill(0);
+    }
+    let faixas = quebra_faixas(consumo, consumo_minimo_agua);
+    let faixas_meta = quebra_faixas(meta, consumo_minimo_agua);
+    let faixas_contingencia = subtraiArrays(faixas, faixas_meta);
+    contingencia_por_faixas = multiplicaArrays(faixas_contingencia, precos_agua);
+    for(let i=0; i < 5; i++) {
+        contingencia_por_faixas[i] *= 1.2;
+    }
+    return contingencia_por_faixas;
+}
+
+
+/**
+ * Calcula o custo de esgoto dado um consumo de água em m3 e o consumo mínimo de
+ * esgoto.
+ *
+ * @param consumo Consumo de água em metros cúbicos.
+ * @param consumo_minimo_agua Consumo mínimo de esgoto.
+ *
+ * @return Custo em reais de esgoto separado por faixa.
+ */
+function calcula_custo_esgoto_por_faixa(consumo, consumo_minimo_esgoto) {
+    let faixas = quebra_faixas(0.8 * consumo, consumo_minimo_esgoto);
+    return  multiplicaArrays(faixas, precos_esgoto);
+}
+
+
+/**
+ * Calcula o custo total de água em reais.
+ *
+ * @param consumo Consumo de água em m3.
+ * @param consumo_minimo_agua Consumo mínimo de água.
+ *
+ * @return Custo total de água.
+ */
+function calcula_custo_agua_somado(consumo, consumo_minimo_agua) {
+    let custo_agua_por_faixa = calcula_custo_agua_por_faixa(consumo, consumo_minimo_agua);
+    return somaArray(custo_agua_por_faixa);
+}
+
+
+/**
+ * Calcula o custo total de esgoto em reais.
+ *
+ * @param consumo Consumo de água em m3.
+ * @param consumo_minimo_agua Consumo mínimo de esgoto.
+ *
+ * @return Custo total de esgoto.
+ */
+function calcula_custo_esgoto_somado(consumo, consumo_minimo_esgoto) {
+    let custo_esgoto_por_faixa = calcula_custo_esgoto_por_faixa(consumo, consumo_minimo_esgoto);
+    return somaArray(custo_esgoto_por_faixa);
+}
---
+++ helpers.js
@@ -0,0 +1,60 @@
+
+"use strict";
+
+/**
+ * Soma os elementos de um array.
+ *
+ * @param inputArray Um array de números.
+ *
+ * @return A soma dos elementos de inputArray.
+ */
+function somaArray(inputArray) {
+    return inputArray.reduce(function(a,b){return a+b;}, 0);
+}
+
+
+/**
+ * Multiplica dois arrays elemento a elemento.
+ *
+ * @param array1 O primeiro array.
+ * @param array2 O segundo array.
+ *
+ * @return Array com o resultado da multiplicação elemento-a-elemento.
+ */
+function multiplicaArrays(array1, array2) {
+    console.assert(array1.length === array1.length, "Os dois arrays devem ter o mesmo tamanho");
+    let total = Array(array1.length).fill(0);
+    for(let i=0; i < array1.length; i++) {
+        total[i] += array1[i] * array2[i];
+    }
+    return total;
+}
+
+
+/**
+ * Subtrai dois arrays.
+ *
+ * @param array1 O primeiro array.
+ * @param array2 O segundo array que será subtraído do primeiro.
+ *
+ * @return array1 - array2 computado elemento a elemento
+ */
+function subtraiArrays(array1, array2) {
+    let resultado = Array(array1.length).fill(0);
+    for(let i = 0; i < array1.length; i++) {
+        resultado[i] = array1[i] - array2[i];
+    }
+    return resultado;
+}
+
+
+/**
+ * Retorna a representação do número como moeda em reais.
+ *
+ * @param numero O valor em reais.
+ *
+ * @return Uma string com R$ antes e duas casas decimais.
+ */
+function emReais(numero) {
+    return numero.toLocaleString(undefined, {style:"currency", currency:"BRL"});
+}
