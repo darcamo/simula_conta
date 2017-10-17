3398f9bd38774ee96827785502fb986b5a543a54
Darlan Cavalcante Moreira <darcamo@gmail.com>
2017-10-17 19:14:13 -0300

    update

--- mychart.js
+++ mychart.js
@@ -1,39 +1,64 @@
-
 function drawChart(minimoAgua, minimoEsgoto, meta) {
-    // let minimoAgua = 10;
-    // let minimoEsgoto = 8;
-    // let meta = 10;
+    //
+    var ctx = document.getElementById("myChartCanvas").getContext('2d');
 
     let labels = [];
     let custo_agua = [];
     let custo_esgoto = [];
     let custo_taxa_extra = [];
 
-    for(let i=10; i < 50; i++) {
+    for(let i=10; i < 40; i++) {
         labels.push(i);
-        custo_agua.push(calcula_custo_agua_somado(i, minimoAgua));
-        custo_esgoto.push(calcula_custo_esgoto_somado(i, minimoEsgoto));
-        let contingencia = somaArray(calcula_custo_agua_excedente_por_faixa(i, minimoAgua, meta));
+        custo_agua.push(roundToTwoDecimals(calcula_custo_agua_somado(i, minimoAgua)));
+        custo_esgoto.push(roundToTwoDecimals(calcula_custo_esgoto_somado(i, minimoEsgoto)));
+        let contingencia = roundToTwoDecimals(somaArray(calcula_custo_agua_excedente_por_faixa(i, minimoAgua, meta)));
         if (contingencia < 0) contingencia = 0;
         custo_taxa_extra.push(contingencia);
     }
 
-    var data = {
-        // A labels array that can contain any sort of values
-        labels: labels,
-        // Our series array that contains series objects or in this case series data arrays
-        series: [custo_agua,
-                 custo_esgoto,
-                 custo_taxa_extra]
-    };
-
     var options = {
-        width: 400,
-        height: 300
+        scales: {
+            yAxes: [{
+                // ticks: {
+                //     beginAtZero:true
+                // },
+                stacked: true
+            }]
+        },
+        responsive: false,
+        animation: {
+            duration: 0 // general animation time
+        }
     };
 
-    // Create a new line chart object where as first parameter we pass in a selector
-    // that is resolving to our chart container element. The Second parameter
-    // is the actual data object.
-    new Chartist.Line('#myChart', data, options);
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [
+                {
+                    label: 'Água',
+                    data: custo_agua,
+                    backgroundColor: 'rgba(132, 99, 255, 0.2)',
+                    borderColor: 'rgba(132, 99, 255, 1)',
+                    borderWidth: 1
+                },
+                {
+                    label: 'Esgoto',
+                    data: custo_esgoto,
+                    backgroundColor: 'rgba(99, 255, 132, 0.2)',
+                    borderColor: 'rgba(99, 255, 132, 1)',
+                    borderWidth: 1
+                },
+                {
+                    label: 'Contingência',
+                    data: custo_taxa_extra,
+                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                    borderColor: 'rgba(255,99,132,1)',
+                    borderWidth: 1
+                }
+            ]
+        },
+        options: options
+    });
 }
